960092ee8f114e0ceab3c12a547d3d697d45501b
BMHDAC <bdachai308@gmail.com>
2024-10-28 20:42:56 +0700

    New BPTree Nodes

--- src/bptree.rs
+++ src/bptree.rs
@@ -5,80 +5,88 @@
 
 use std::sync::Arc;
 
-#[derive(Debug, Clone)]
-pub struct BPTree<T: Clone + std::fmt::Debug>(pub Arc<Node<T>>);
+const TREE_BASE: usize = 6;
 
-#[derive(Debug, Clone)]
-pub enum Node<T>
-where
-    T: Clone + std::fmt::Debug,
-{
-    Leaf { values: Vec<T> },
-    Internal { height: u8, childs: Vec<BPTree<T>> },
+#[derive(Clone)]
+pub struct BPTree<T: TreeIdx>(Arc<Node<T>>);
+
+#[derive(Clone)]
+pub enum Node<T: TreeIdx> {
+    Internal {
+        height: u8,
+        childs: Vec<BPTree<T>>,
+        index: T::Index,
+        child_indexes: Vec<T::Index>,
+    },
+    Leaf {
+        index: T::Index,
+        item_indexes: Vec<T::Index>,
+        items: Vec<T>,
+    },
 }
 
-impl<T> Node<T>
-where
-    T: Clone + std::fmt::Debug,
-{
+impl<T: TreeIdx> Node<T> {
     pub fn is_leaf(&self) -> bool {
         matches!(self, Self::Leaf { .. })
     }
 
-    pub fn childs(&self) -> &[BPTree<T>] {
-        match self {
-            Node::Leaf { .. } => &[],
-            Node::Internal { childs, .. } => childs,
-        }
-    }
-
-    pub fn values(&self) -> &[T] {
-        match self {
-            Node::Leaf { values } => values,
-            Node::Internal { .. } => &[],
-        }
-    }
-
     pub fn height(&self) -> u8 {
         match self {
-            Node::Leaf { .. } => 0,
             Node::Internal { height, .. } => *height,
+            Node::Leaf { .. } => 0,
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        match self {
+            Node::Internal { childs, .. } => childs.is_empty(),
+            Node::Leaf { items, .. } => items.is_empty(),
+        }
+    }
+
+    pub fn child_indexes(&self) -> &[T::Index] {
+        match self {
+            Node::Internal { child_indexes, .. } => child_indexes.as_slice(),
+            Node::Leaf { item_indexes, .. } => item_indexes.as_slice(),
+        }
+    }
+
+    pub fn child_tree(&self) -> &[BPTree<T>] {
+        match self {
+            Node::Internal { childs, .. } => childs.as_slice(),
+            Node::Leaf { .. } => Default::default(),
+        }
+    }
+
+    pub fn items(&self) -> &[T] {
+        match self {
+            Node::Internal { .. } => Default::default(),
+            Node::Leaf { items, .. } => items.as_slice(),
+        }
+    }
+
+    pub fn is_underflowing(&self) -> bool {
+        match self {
+            Node::Internal { childs, .. } => childs.len() < TREE_BASE,
+            Node::Leaf { items, .. } => items.len() < TREE_BASE,
         }
     }
 }
 
-impl<T> BPTree<T>
+impl<T: TreeIdx> BPTree<T>
 where
     T: Clone + std::fmt::Debug,
 {
     pub fn new() -> Self {
-        Self(Arc::new(Node::Leaf { values: Vec::new() }))
-    }
-
-    pub fn first(&self) -> Option<&T> {
-        Self::walk_left(self)
-    }
-
-    pub fn last(&self) -> Option<&T> {
-        Self::walk_right(self)
-    }
-
-    fn walk_left(node: &Self) -> Option<&T> {
-        match node.0.as_ref() {
-            Node::Leaf { values } => values.first(),
-            Node::Internal { childs, .. } => Self::walk_left(childs.first()?),
-        }
-    }
-
-    fn walk_right(node: &Self) -> Option<&T> {
-        match node.0.as_ref() {
-            Node::Leaf { values } => values.last(),
-            Node::Internal { childs, .. } => Self::walk_right(childs.last()?),
-        }
+        Self(Arc::new(Node::Leaf {
+            index: Default::default(),
+            item_indexes: Vec::default(),
+            items: Vec::default(),
+        }))
     }
 }
 
-impl<T> Default for BPTree<T>
+impl<T: TreeIdx> Default for BPTree<T>
 where
     T: Clone + std::fmt::Debug,
 {
@@ -87,10 +95,12 @@ where
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub trait TreeIdx: Clone + Default {
+    type Index: Summary;
 
-    #[test]
-    fn test_append_tree() {}
+    fn index(&self) -> Self::Index;
+}
+
+pub trait Summary: Clone + Default {
+    fn add_summary(&mut self, other: Self);
 }
